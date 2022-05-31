42c08765bcb7228611d0dd53ffb1e06ba0c5c890
DashaEvstratova <alis131140@gmail.com>
2022-05-31 18:39:22 +0300

    masking

--- src/bits.cpp
+++ src/bits.cpp
@@ -6,7 +6,8 @@ namespace assignment {
 
   bool is_bit_set(int mask, int pos) {
     assert(mask >= 0 && pos >= 0 && pos < 30);
-    return false;
+    const int check_mask = 1 << pos;
+    return (mask & check_mask) != 0;
   }
 
   int set_bit(int mask, int pos) {
